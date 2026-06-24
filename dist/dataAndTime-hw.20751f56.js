0b62c435e82330fbeb0ce7b002862226fdf06a3a
Коля <135145397+Kolya4954@users.noreply.github.com>
2026-06-25 00:27:11 +0300

    bruh

---
+++ dist/dataAndTime-hw.20751f56.js
@@ -0,0 +1,2 @@
+document.querySelector(".heading"),document.querySelector("[data-value='days']"),document.querySelector("[data-value='hours']"),document.querySelector("[data-value='mins']"),document.querySelector("[data-value='secs']");let e=new Date(2026,7,1);setInterval(()=>{if(e-new Date<=0)return void clearInterval(id)},1e3);
+//# sourceMappingURL=dataAndTime-hw.20751f56.js.map
