7a07135f8dba473b814d308e1b9c61ee97dac041
Коля <135145397+Kolya4954@users.noreply.github.com>
2026-06-27 15:48:21 +0300

    final fix

--- src/main.js
+++ src/main.js
@@ -1,37 +1,29 @@
-const headingRef = document.querySelector(".heading")
+
 const daysRef = document.querySelector("[data-value='days']");
 const hoursRef = document.querySelector("[data-value='hours']");
 const minutesRef = document.querySelector("[data-value='mins']");
 const secondsRef = document.querySelector("[data-value='secs']");
 
 
-const neededTime = new Date(2026, 7, 1)
+const neededTime = new Date(2026, 11, 31, 23, 59, 59)
 
 
 
 const idi = setInterval(() =>{
     const timeNow = new Date()
     
-    const time = neededTime - timeNow
-    if(time <= 0){
-    clearInterval(id)
-    return
+    const time = (neededTime - timeNow);
 
-
-    headingRef.textContent = "second month of summer has arrived!"
-
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((time % (1000 * 60)) / 1000);
 
     daysRef.textContent = days
     hoursRef.textContent = hours
     minutesRef.textContent = mins
-    secondsRef.textContent = secondsRef
-}
-
-
-
- const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+    secondsRef.textContent = secs
 
 },1000);
+
+setInterval(() => {},1000)
